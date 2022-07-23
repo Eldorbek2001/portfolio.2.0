dc71065dbb219253022d0f9ddee595c53b315fbf
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-07-23 12:58:45 -0700

    add some css, use ejs

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// JShint esversion: 6
+require('dotenv').config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app =  express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname+"/index.html");
+  res.render('core', {content: 'landing'});
+});
+
+app.get("/skills", function(req, res){
+  res.render('core', {content: 'skills'});
+});
+
+
+
+app.get("/education", function(req, res){
+  res.render('core', {content: 'education'});
 });
 
 
