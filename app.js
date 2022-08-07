e89cb358ddbfb01868957272dc2331c66fefccb1
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-08-07 01:16:13 -0700

    set up mongoose, link database

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app =  express();
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://"+process.env.DATABASE_API_USERNAME+":"+process.env.DATABASE_API_PASSWORD+"@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority/testDB/test", {useNewUrlParser: true});
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+var newsSchema = {
+author: String,
+title: String,
+date: Object,
+content: String,
+source: String,
+}
+
+var alumniSchema = {
+fname: String,
+lname: String,
+mail: String,
+photoPath: String,
+role: String
+}
+var authSchema = {
+  email: String,
+  pwd: String
+}
+const User = mongoose.model("auth", authSchema);
+const News = mongoose.model("news", newsSchema);
+const Alumni = mongoose.model("users", alumniSchema);
+console.log(mongoose);
 
 app.get("/", function(req, res){
   res.render('core', {content: 'landing'});
