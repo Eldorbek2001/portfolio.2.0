818aefc910df8fe2551f26233050d755dfc64168
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2023-04-30 15:00:00 -0700

    added a project

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app =  express();
 var nodemailer = require('nodemailer');
-//const mongoose = require("mongoose");
-//mongoose.connect("mongodb+srv://"+process.env.DATABASE_API_USERNAME+":"+process.env.DATABASE_API_PASSWORD+"@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority/testDB/test", {useNewUrlParser: true});
-//const { Telegraf } = require('telegraf');
-//const bot = new Telegraf(process.env.BOT_TOKEN);
-//const telegramHandler = require(__dirname + "/views/telegramHandler.js");
+
 
 
 app.use(express.static("public"));
@@ -17,14 +13,6 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-
-//
-// bot.on("message", (ctx) =>{
-//   var res = telegramHandler.getLink(ctx.message);
-//   console.log(res);
-// })
-// bot.launch();
-//
 
 var newsSchema = {
 author: String,
@@ -50,24 +38,25 @@ var authSchema = {
 app.get("/", function(req, res){
 
   res.render("core", {content: "landing"})
-  //res.sendFile(__dirname+"/pages/home.html");
 });
 
 
 
 app.get("/skills", function(req, res){
     res.render("core", {content: "skills"})
-  // res.sendFile(__dirname+"/pages/skills.html");
+
 });
 
 
 
 app.get("/education", function(req, res){
     res.render("core", {content: "education"})
-// res.sendFile(__dirname+"/pages/education.html");
+
 });
 
 
 
 app.listen(process.env.PORT || 3000, function(){
+console.log("here" ) ;
+
 });
