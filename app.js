e94d2dfa81b15427916cf1a7052a63e3159a05aa
Ali <83210137+alielbekov@users.noreply.github.com>
2023-07-31 20:40:28 +0000

    fix overlapping info

--- app.js
+++ app.js
@@ -1,56 +1,49 @@
-require('dotenv').config();
-const bot = require('./bot');
-const alumni_bot = require('./alumni_bot');
+require("dotenv").config();
+const bot = require("./bot");
+const alumni_bot = require("./alumni_bot");
 
-const axios = require('axios');
+const axios = require("axios");
 const bodyParser = require("body-parser");
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
-const fs = require('fs');
-const readline = require('readline');
-const mongoose = require('mongoose');
+const fs = require("fs");
+const readline = require("readline");
+const mongoose = require("mongoose");
 const express = require("express");
 
-
-
-const githubAPI = "https://api.github.com/repos/neetcode-gh/leetcode/contents/python/";
+const githubAPI =
+  "https://api.github.com/repos/neetcode-gh/leetcode/contents/python/";
 const app = express();
-
-
 
 var currentDay = 0;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_KEY}@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, writeConcern: { w: 'majority', j: true, wtimeout: 1000 } });
-
+app.set("view engine", "ejs");
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_KEY}@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: { w: "majority", j: true, wtimeout: 1000 },
+  }
+);
 
 bot.launch();
 alumni_bot.launch();
 
-
-
-app.get("/", function(req, res){
-  res.render("core", {content: "landing"})
+app.get("/", function (req, res) {
+  res.render("core", { content: "landing" });
 });
 
-
-
-app.get("/skills", function(req, res){
-    res.render("core", {content: "skills"})
+app.get("/skills", function (req, res) {
+  res.render("core", { content: "skills" });
 });
 
-
-app.get("/education", function(req, res){
-    res.render("core", {content: "education"})
-
+app.get("/education", function (req, res) {
+  res.render("core", { content: "education" });
 });
 
-
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log("listening on port 3000");
-
 });
-
-
