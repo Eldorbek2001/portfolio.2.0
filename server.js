b815e7616083d25a793b074247e6d93dfd765efd
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2024-11-11 15:22:26 -0800

    update some stuff

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 require('dotenv').config();
 
-const axios = require("axios");
+
 const bodyParser = require("body-parser");
-const cheerio = require("cheerio");
 
-const fs = require("fs");
-const readline = require("readline");
-const mongoose = require("mongoose");
 const express = require("express");
-
-const githubAPI =
-  "https://api.github.com/repos/neetcode-gh/leetcode/contents/python/";
 const app = express();
 
-var currentDay = 0;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_KEY}@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, writeConcern: { w: 'majority', j: true, wtimeout: 1000 } });
 
 
 
